de7a1745ca7246e1fab9b557a402c4144b9fe725
khankadir193 <abdul.kadir2019@vitbhopal.ac.in>
2024-01-31 15:54:43 +0530

    integrationOCR to backend

--- src/components/OCRComponents.vue
+++ src/components/OCRComponents.vue
@@ -16,21 +16,51 @@ export default {
     },
     methods: {
         handleUploadFile(event) {
-            console.log('event...???',event.target.files[0]);
-            const file = event.target.files[0].name; 
-            if(file){
-                const reader = new FileReader();
-                reader.onload = (e)=>{
-                    console.log('e.target.result',e.target.result);
-                    this.imgUrl = e.target.result;
-                    this.IntegrationOCR();
-                }
-            // reader.readDataURL(file);
-
+            console.log('event...???', event.target.files[0]);
+            const file = event.target.files[0].name;
+            if (file)
+                console.log('file is true...')
+            if (file) {
+                const reader = new FileReader(file);
+                console.log('reader...??', reader);
+                this.IntegrationOCR();
+                // reader.onload = (e) => {
+                //     console.log('e.target.result', e.target.result);
+                //     this.imgUrl = e.target.result;
+                //     this.IntegrationOCR();
+                // }
+                // reader.readAsDataURL(file);
             }
         },
-        IntegrationOCR(){
-            console.log('backend-ocr integration');
+        IntegrationOCR() {
+            // const serverEndPoint = "http://localhost:3000/perform-ocr"
+            // // console.log('jjj')
+            // fetch(serverEndPoint, {
+            //     method: 'POST', headers: { 'content-type': 'application/json' },
+            //     body: JSON.stringify({ imgUrl: this.imgUrl })
+            // }).then((response)=>{
+            //     response.json();
+            // }).then((data)=>{
+            //     console.log('data...!!!???',data);
+            //     this.recognizedText = data;
+            // });
+
+            const serverEndPoint = "http://localhost:3000/perform-ocr";
+
+            fetch(serverEndPoint, {
+                method: 'POST',
+                headers: { 'content-type': 'application/json' },
+                body: JSON.stringify({ imgUrl: "abdulkadirkhan" })
+            })
+                .then((response) => response.json()) // Add return statement here
+                .then((data) => {
+                    console.log('data...!!!???', data);
+                    this.recognizedText = data;
+                })
+                .catch((error) => {
+                    console.error('Error during fetch:', error);
+                });
+
         }
     }
 
