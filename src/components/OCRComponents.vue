1e9f7d336686b904339bccc7c2b06cd3b6245c10
khankadir193 <abdul.kadir2019@vitbhopal.ac.in>
2024-02-01 15:35:46 +0530

    this code has been working for extracting the data from image

--- src/components/OCRComponents.vue
+++ src/components/OCRComponents.vue
@@ -1,72 +1,56 @@
 <template>
     <div>
-        <input type="file" @change="handleUploadFile" />
-        <img :src="imgUrl" alt="Uploaded Image">
+      <input type="file" @change="handleUploadFile" />
+      <img :src="imgUrl" alt="Uploaded Image" />
+      <div>{{ recognizedText }}</div>
     </div>
-</template>
-
-<script>
-// import axios from 'axios';
-export default {
+  </template>
+  
+  <script>
+  export default {
     name: 'OCRComponents',
     data() {
-        return {
-            imgUrl: 'https://images.pexels.com/photos/674010/pexels-photo-674010.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
-            recognizedText: ''
-        }
+      return {
+        imgUrl: '',
+        recognizedText: '',
+      };
     },
     methods: {
-        handleUploadFile(event) {
-            console.log('event...???', event.target.files[0]);
-            const file = event.target.files[0].name;
-            if (file)
-                console.log('file is true...')
-            if (file) {
-                const reader = new FileReader(file);
-                console.log('reader...??', reader);
-                this.IntegrationOCR();
-                // reader.onload = (e) => {
-                //     console.log('e.target.result', e.target.result);
-                //     this.imgUrl = e.target.result;
-                //     this.IntegrationOCR();
-                // }
-                // reader.readAsDataURL(file);
-            }
-        },
-        IntegrationOCR() {
-            // const serverEndPoint = "http://localhost:3000/perform-ocr";
-            // fetch(serverEndPoint, {
-            //     method: 'POST',
-            //     headers: { 'content-type': 'application/json' },
-            //     body: JSON.stringify({ imgUrl: "abdulkadirkhan" })
-            // }).then((response) => response.json()) // Add return statement here
-            //     .then((data) => {
-            //         console.log('data...!!!???', data);
-            //         this.recognizedText = data;
-            //     }).catch((error) => {
-            //         console.error('Error during fetch:', error);
-            //     });
-            const serverEndPoint = 'http://localhost:3000/perform-ocr';
-            fetch(serverEndPoint, {
-                method: 'POST',
-                mode:'no-cors',
-                headers: {
-                    'Content-Type': 'application/json',
-                },
-                body: JSON.stringify({ imgUrl: this.imgUrl }),
-            }).then((response) => response.json()).then((data) => {
-                    // console.log('OCR Result:', data.recognizedText);
-                    // this.recognizedText = data.recognizedText;
-                    console.log('recognise text.......????????',data);
-                }).catch((error) => {
-                    console.error('Error during fetch:', error);
-                });
+      handleUploadFile(event) {
+        const file = event.target.files[0];
+        if (file) {
+          const reader = new FileReader();
+          reader.onload = (e) => {
+            this.imgUrl = e.target.result;
+            this.IntegrationOCR(file);
+          };
+          reader.readAsDataURL(file);
         }
-    }
+      },
+      async IntegrationOCR(file) {
+        const formData = new FormData();
+        formData.append('image', file);
+  
+        // Use axios or fetch to send the image data to the server
+        // Replace the placeholder URL with your actual server endpoint
+        const serverEndPoint = 'http://localhost:3000/perform-ocr';
+        await fetch(serverEndPoint, {
+          method: 'POST',
+          body: formData,
+        })
+          .then((response) => response.json())
+          .then((data) => {
+            console.log('Recognized Text:', data.text);
+            this.recognizedText = data.text;
+          })
+          .catch((error) => {
+            console.error('Error during fetch:', error);
+        });
 
-}
-
-</script>
-
-
-<style scoped></style>
+      },
+    },
+  };
+  </script>
+  
+  <style scoped></style>
+  
